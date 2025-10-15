2bf5d09ce3b920a0dd747e0c2ed6c88548550544
IgLua <iluarca@visiion.cl>
2025-10-15 17:05:53 -0300

    Cambios en nueva navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,48 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('products.index') }}">Mi CRUD</a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->routeIs('products.*') ? 'active' : '' }}" 
+                       href="{{ route('products.index') }}">
+                       Productos
+                    </a>
+                </li>
+
+                @auth
+                    @if (Auth::user()->is_admin)
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->routeIs('users.*') ? 'active' : '' }}" 
+                               href="{{ route('users.index') }}">
+                               Usuarios
+                            </a>
+                        </li>
+                    @endif
+                @endauth
+            </ul>
+
+            <ul class="navbar-nav ms-auto">
+                @auth
+                    <li class="nav-item d-flex align-items-center me-3">
+                        <span class="text-white">{{ Auth::user()->name }}</span>
+                    </li>
+
+                    <li class="nav-item ms-2">
+                        <form action="{{ route('logout') }}" method="POST" class="d-inline">
+                            @csrf
+                            <button type="submit" class="btn btn-danger btn-sm">
+                                Cerrar sesión
+                            </button>
+                        </form>
+                    </li>
+                @endauth
+            </ul>
+        </div>
+    </div>
+</nav>
